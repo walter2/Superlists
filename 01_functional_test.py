702c69a2366ce2a54bac5960f3a2c98622d968f7
Walter Kummer <work.walter@gmail.com>
2014-08-03 16:51:15 +0100

    POST requests are now saved in data base; Redirect after POST added, Tests refactored. p. 69

--- 01_functional_test.py
+++ 01_functional_test.py
@@ -1,15 +1,71 @@
-#!/usr/bin/env python3
-# 01_functional_test.py
-#2014-08-03
+#!/usr/bin/env python
+#01_functional_test.py
 
-__author__ = 'Walter Kummer'
-
+import unittest
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
-if __name__ == "__main__":
-    pass
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Jimmy checks out the web page at:
+        self.browser.get('http://localhost:8000')
+
+        #He notes that the header is called.
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        #He is invited to enter some tasks into the text box.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        #He types 'Cut the grass in the garden'.
+        inputbox.send_keys('Cut the grass in the garden')
+
+        #When he hits enter, the page updates and shows:
+        #'1. Cut the grass in the garden' as an item in the ToDo list.
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Cut the grass in the garden')
+
+        #The text box is inviting him to write more
+        #He enters: 'Put the cut grass into the wheelie bin.'
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Put the cut grass into the wheelie bin.')
+        inputbox.send_keys(Keys.ENTER)
+
+        #The page updates and shows now both entries.
+        self.check_for_row_in_list_table('1: Cut the grass in the garden')
+        self.check_for_row_in_list_table('2: Put the cut grass into the wheelie bin.')
+
+
+        #Jimmy wonders if the site will remember the items - the site has generated an unique url for him
+        #after a reload of the url the items are still there, the unique url is explained.
+        self.fail('Finish the tests now here.')
+
+        #Jimmy moves on to different things.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+#log
+#2014-07-31
+#- first test for Test-Django book, p. 6
+#- initial user stories added, p. 14
+#- user is called Jimmy
+#- test implemented as unit test, p. 16
--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,7 @@ from lists.models import Item
 
 from lists.views import home_page
 
-class HomePageTEst(TestCase):
+class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -21,14 +21,23 @@ class HomePageTEst(TestCase):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
-
         response = home_page(request)
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+
+    def test_home_page_redirects_after_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
