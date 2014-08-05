b9b84a554f9b095969fe29d4d15fb21f611225a6
Walter Kummer <work.walter@gmail.com>
2014-08-05 14:14:31 +0100

    Cleaned up list/test.py; added new url for lists/new to urlpy; added new_list() function to views.py; added 'action' to home.html and list.html; updated redirect. Chapter 6, p. 96

--- superlists/urls.py
+++ superlists/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
      url(r'^$', 'lists.views.home_page', name='home'),
      url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
          name = 'view_list'),
-
+     url(r'^lists/new$', 'lists.views.new_list', name = 'new_list')
     #url(r'^admin/', include(admin.site.urls)),
     )
