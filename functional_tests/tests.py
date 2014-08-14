13e918ada2edb588b79592173a60b84dc5852d92
Walter Kummer <work.walter@gmail.com>
2014-08-14 18:02:17 +0100

    Centered textinbox layout in lists/templates/home.html. Functional test accordingly updated. Few urls.py updates from last chapter. Chapter 7, p. 118

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -86,6 +86,26 @@ class NewVisitTest(LiveServerTestCase):
 
         #Jimmy and Peter moves on to different things.
 
+    def test_layout_and_styling(self):
+        #Jimmy goes to the home page and
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #He notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2,
+                               512, delta=10)
+
+        #When a new list is started the input box is centered
+        # there as well
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2,
+                               512, delta=10)
+
+
+
+
 #log
 #2014-07-31
 #- first test for Test-Django book, p. 6
--- lists/tests.py
+++ lists/tests.py
@@ -66,7 +66,7 @@ class ListViewTest(TestCase):
     def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
-        response = self.client.get('/list/%d/' % (correct_list.id,))
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
         self.assertEqual(response.context['list'], correct_list)
 
 class NewListTest(TestCase):
