14edd4b37ffe7fcef07c03fe46ff4fd007a79fdc
Walter <work.walter@gmail.com>
2014-09-10 18:43:11 +0100

    lists/templates/base.html updated with error message posting. Chapter 10, page 182

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from lists.models import Item, List
 
@@ -12,7 +13,14 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text = request.POST['item_text'], list = list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = 'You cannot have an empty list item.'
+        return render(request, 'home.html', {'error': error})
     return redirect('/lists/%d/' % (list_.id,))
 
 
