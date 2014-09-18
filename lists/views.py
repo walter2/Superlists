29f016c9a1a50a7654a0e048c1157190643ba871
Walter <work.walter@gmail.com>
2014-09-18 17:11:04 +0100

    Refactored list view to handle new item POSTs. Machted lists/tests with Harry's github rep. Chapter 10, p. 185

--- lists/views.py
+++ lists/views.py
@@ -5,15 +5,9 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-
-def view_list(request, list_id):
-    list_ = List.objects.get(id = list_id)
-    return render(request, 'list.html', {'list': list_})
-
-
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -28,3 +22,10 @@ def add_item(request, list_id):
     list_ = List.objects.get(id = list_id)
     Item.objects.create(text = request.POST['item_text'], list = list_)
     return redirect('/lists/%d/' % (list_.id,))
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id = list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
+    return render(request, 'list.html', {'list': list_})
