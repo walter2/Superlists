175e6cab314e3172998d31895ffa27fc09bace4b
Walter <work.walter@gmail.com>
2014-09-30 08:14:16 +0100

    Added form save() in lists/forms.py. Updated lists/views.py as well. Test currently fail due to base class on Windows. Chapter 11, p. 210

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -26,7 +26,7 @@ class ItemValidationTest(FunctionalTest):
         #He receives a similar error page like on the list page
         self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You cannot have an empty list item.")
+        self.assertEqual(error.text, "You cannot have an empty list item")
 
         #He can correct it by filling some text in
         self.get_item_input_box().send_keys('Make tea\n')
